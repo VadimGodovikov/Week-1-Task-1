34c16a974d7176f6db2e383ffc00ed09999cc192
VadimGodovikov <119358867+VadimGodovikov@users.noreply.github.com>
2022-12-02 01:34:38 +0300

    git add
    Неделя 1 Задание 1

---
+++ prog1.2graf/prog1.2graf/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace prog1._2graf
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 26);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(147, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Введите двухзначное число";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(16, 92);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(59, 13);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Результат";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(116, 92);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(274, 78);
+			this.textBox2.TabIndex = 3;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(16, 146);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "Пуск";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(202, 26);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            1215752192,
+            23,
+            0,
+            0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(188, 20);
+			this.numericUpDown1.TabIndex = 5;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(513, 240);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+	}
+}
+
---
+++ zadanie1.1/zadanie1.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie1._1
+{
+	class Program
+	{
+		private static double readDouble()
+		{
+			do
+			{
+				double result;
+				bool parsed = double.TryParse(Console.ReadLine(), out result);
+				if (parsed) return result;
+				else Console.WriteLine("Введите корректное число");
+			} while (true);
+		}
+		static void Main(string[] args)
+		{
+
+			v1: Console.WriteLine("Введите объём куба: ");
+			double v = readDouble();
+
+			if(v <= 0)
+			{
+				Console.WriteLine("Объём куба не может быть равен нулю и быть меньше нуля.");
+				goto v1;
+			}
+
+			double a = Math.Pow(v, 1.0/3.0);
+			Console.WriteLine($"Ребро куба равняется: {a}");
+
+		}
+	}
+}
